ecc8d23ecac1c584b82533287678502234a5474a
Paszinski <lucaspaszinski@gmail.com>
2021-07-31 13:50:47 -0300

    Remove unused code

--- lib/nfl_web/live/components/pagination.ex
+++ lib/nfl_web/live/components/pagination.ex
@@ -4,46 +4,42 @@ defmodule NflWeb.Live.Components.Pagination do
   """
   use NflWeb, :live_component
 
-  def pages_sizes do
-    ~w(10 25 50 100 500 1000)
-  end
-
   def render(assigns) do
     ~L"""
     <nav>
-     <ul class="pagination horizontal">
-       <li>
-         <a
+      <ul class="pagination horizontal">
+        <li>
+          <a
             href="#"
             phx-click="nav"
             phx-value-page="<%= if @page > 1 , do: @page - 1, else: @page %>"
           >
             <
           </a>
-       </li>
+        </li>
     <%= for idx <- 1..@total_pages do %>
-       <li>
-        <a
-          href="#"
-          phx-click="nav"
-          phx-value-page="<%= idx %>"
-          class="<%= if @page == idx, do: "active" %>"
-        >
-          <%= idx %>
-        </a>
-       </li>
-    <% end %>
-       <li>
-         <a
-         href="#"
-         phx-click="nav"
-         phx-value-page="<%= if @page < @total_pages , do: @page - 1, else: @page %>"
-         >
+        <li>
+          <a
+            href="#"
+            phx-click="nav"
+            phx-value-page="<%= idx %>"
+            class="<%= if @page == idx, do: "active" %>"
           >
-        </a>
-       </li>
-     </ul>
-     </nav>
+            <%= idx %>
+          </a>
+        </li>
+    <% end %>
+        <li>
+          <a
+            href="#"
+            phx-click="nav"
+            phx-value-page="<%= if @page < @total_pages , do: @page - 1, else: @page %>"
+          >
+          >
+          </a>
+        </li>
+      </ul>
+    </nav>
     """
   end
 end
